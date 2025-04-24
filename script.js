72324c9f885f8ce2e446f5c3667d810868544160
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-04-24 12:19:59 +0100

    Shorten for loop in getChildren.

--- script.js
+++ script.js
@@ -1,74 +1,70 @@
-
 function getChildren(square) {
+  array = [];
+  const h = square[0];
+  const v = square[1];
 
-    array = [];
-    const h = square[0];
-    const v = square[1];
+  array.push([h + 1, v + 2]);
+  array.push([h + 2, v + 1]);
+  array.push([h + 2, v - 1]);
+  array.push([h + 1, v - 2]);
+  array.push([h - 1, v - 2]);
+  array.push([h - 2, v - 1]);
+  array.push([h - 2, v + 1]);
+  array.push([h - 1, v + 2]);
 
-    array.push([h + 1, v + 2]);
-    array.push([h + 2, v + 1]);
-    array.push([h + 2, v - 1]);
-    array.push([h + 1, v - 2]);
-    array.push([h - 1, v - 2]);
-    array.push([h - 2, v -1]);
-    array.push([h -2, v + 1]);
-    array.push([h - 1, v + 2]);
-
-    for (let i = array.length - 1; i >= 0; i--) {
-        if ((array[i][0] < 0) || (array[i][1] < 0)) {
-            array.splice(i, 1);
-        }
-        else if ((array[i][0] > 7) || (array[i][1] > 7)) {
-            array.splice(i, 1);
-        }
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (
+      array[i][0] < 0 ||
+      array[i][1] < 0 ||
+      array[i][0] > 7 ||
+      array[i][1] > 7
+    ) {
+      array.splice(i, 1);
     }
+  }
 
-    return array;
+  return array;
 }
 
 function knightMoves(start, target) {
-    let route = [];
-    route.push(start);
-    const targetChildren = getChildren(target);
+  let route = [];
+  route.push(start);
+  const targetChildren = getChildren(target);
 
-    const checkChildren = (square) => {
+  const checkChildren = (square) => {
+    const children = getChildren(square);
 
-        const children = getChildren(square);
-
-        for (const child of children) {
-            if (child[0] === target[0] && child[1] === target[1]) {
-                route.push(target);
-                return
-            }
-            for (const targetChild of targetChildren) {
-                if (child[0] == targetChild[0] && child[1] == targetChild[1]) {
-                    route.push(targetChild);
-                    route.push(target);
-                    return
-                }
-            }
+    for (const child of children) {
+      if (child[0] === target[0] && child[1] === target[1]) {
+        route.push(target);
+        return;
+      }
+      for (const targetChild of targetChildren) {
+        if (child[0] == targetChild[0] && child[1] == targetChild[1]) {
+          route.push(targetChild);
+          route.push(target);
+          return;
         }
-
-        let currentDifference = 14;
-        let nextSquare;
-
-        for (const child of children) {
-            const horizontalDifference = Math.abs(child[0] - target[0]);
-            const verticalDifference = Math.abs(child[1] - target[1]);
-            if (horizontalDifference + verticalDifference < currentDifference) {
-                currentDifference = horizontalDifference + verticalDifference;
-                nextSquare = child;
-            }
-        }
-
-        route.push(nextSquare);
-        checkChildren(nextSquare);
+      }
     }
 
-    checkChildren(start);
+    let currentDifference = 14;
+    let nextSquare;
 
-    return route;
+    for (const child of children) {
+      const horizontalDifference = Math.abs(child[0] - target[0]);
+      const verticalDifference = Math.abs(child[1] - target[1]);
+      if (horizontalDifference + verticalDifference < currentDifference) {
+        currentDifference = horizontalDifference + verticalDifference;
+        nextSquare = child;
+      }
     }
 
-knightMoves([0, 0],[7, 6]);
+    route.push(nextSquare);
+    checkChildren(nextSquare);
+  };
 
+  checkChildren(start);
+
+  return route;
+}
